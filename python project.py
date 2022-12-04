ee8276f7f906c0464359f198f9c8eea7bab01ad8
ssr543 <ssrathor543@gmail.com>
2022-12-04 15:41:41 +0530

    first commit

---
+++ python project.py
@@ -0,0 +1,12 @@
+class table:
+    def tab(self):
+        n=int(input("Enter a no.: "))
+        for i in range(2,n+1):
+            for j in range (1,11):
+                print(i,'*',j,'=', i*j)
+
+
+a=table()
+a.tab()
+
+
